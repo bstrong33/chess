614f90ddd56c58265a4e02210d3ca46b34e5dbac
bstrong33 <boston.strong33@gmail.com>
2022-07-18 14:48:59 -0400

    added logic for pawn movement and capturing

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -17,8 +17,8 @@ function Board() {
 
   useEffect(() => {
     // console.log(squares);
-    // console.log(capturedPieces);
-    console.log(selectedPiece);
+    console.log(capturedPieces);
+    // console.log(selectedPiece);
   }, [squares]);
 
   // Runs all movement for board
@@ -110,10 +110,12 @@ function Board() {
   }
 
   function checkPieceMovement(index) {
-    let newSquareColumn = parseInt(squares[index].column);
+    let newSquareColumn = squares[index].column;
     let newSquareRow = squares[index].row;
-    let pieceColumn = parseInt(selectedPiece.column);
+    let pieceColumn = selectedPiece.column;
     let pieceRow = selectedPiece.row;
+    let pieceColor = selectedPiece.color;
+    let pieceMoved = selectedPiece.hasMoved;
 
     let piece = selectedPiece.piece;
     if (piece === "pawn") {
@@ -122,9 +124,12 @@ function Board() {
         newSquareColumn,
         newSquareRow,
         pieceColumn,
-        pieceRow
+        pieceRow,
+        pieceColor,
+        pieceMoved
       );
     } else if (piece === "knight") {
+      return true;
     } else if (piece === "bishop") {
     } else if (piece === "rook") {
     } else if (piece === "queen") {
@@ -137,14 +142,107 @@ function Board() {
     newSquareColumn,
     newSquareRow,
     pieceColumn,
-    pieceRow
+    pieceRow,
+    pieceColor,
+    pieceMoved
   ) {
-    // console.log(_.isEqual(newSquareColumn, pieceColumn));
-    if (newSquareColumn === pieceColumn) {
-      return true;
-    } else {
-      return false;
+    // Checks color of the pawn, makes sure the pawn stays in the same column, and can't move to a square that has a piece already
+    if (pieceColor === "white") {
+      if (
+        pieceMoved &&
+        newSquareRow - 1 === pieceRow &&
+        newSquareColumn === pieceColumn &&
+        squares[index].hasPiece === false
+      ) {
+        return true;
+      } else if (
+        !pieceMoved &&
+        newSquareRow === 3 &&
+        newSquareColumn === pieceColumn &&
+        squares[index].hasPiece === false
+      ) {
+        return true;
+      } else if (
+        !pieceMoved &&
+        newSquareRow === 4 &&
+        newSquareColumn === pieceColumn &&
+        squares[index].hasPiece === false
+      ) {
+        let pieceInTheWay = false;
+        for (let i = 0; i < squares.length; i++) {
+          if (
+            squares[i].row === 3 &&
+            squares[i].column === pieceColumn &&
+            squares[i].hasPiece
+          ) {
+            pieceInTheWay = true;
+          }
+        }
+        return !pieceInTheWay;
+      } else if (
+        newSquareColumn - 1 === pieceColumn &&
+        newSquareRow - 1 === pieceRow &&
+        squares[index].hasPiece
+      ) {
+        return true;
+      } else if (
+        newSquareColumn + 1 === pieceColumn &&
+        newSquareRow - 1 === pieceRow &&
+        squares[index].hasPiece
+      ) {
+        return true;
+      }
+    } else if (pieceColor === "black") {
+      if (
+        pieceMoved &&
+        newSquareRow + 1 === pieceRow &&
+        newSquareColumn === pieceColumn &&
+        squares[index].hasPiece === false
+      ) {
+        return true;
+      } else if (
+        !pieceMoved &&
+        newSquareRow === 6 &&
+        newSquareColumn === pieceColumn &&
+        squares[index].hasPiece === false
+      ) {
+        return true;
+      } else if (
+        !pieceMoved &&
+        newSquareRow === 5 &&
+        newSquareColumn === pieceColumn &&
+        squares[index].hasPiece === false
+      ) {
+        let pieceInTheWay = false;
+        for (let i = 0; i < squares.length; i++) {
+          if (
+            squares[i].row === 6 &&
+            squares[i].column === pieceColumn &&
+            squares[i].hasPiece
+          ) {
+            pieceInTheWay = true;
+          }
+        }
+        return !pieceInTheWay;
+      } else if (
+        newSquareColumn - 1 === pieceColumn &&
+        newSquareRow + 1 === pieceRow &&
+        squares[index].hasPiece
+      ) {
+        return true;
+      } else if (
+        newSquareColumn + 1 === pieceColumn &&
+        newSquareRow + 1 === pieceRow &&
+        squares[index].hasPiece
+      ) {
+        return true;
+      }
     }
+    // if (newSquareColumn === pieceColumn && newSquareRow - 1 === pieceRow) {
+    //   return true;
+    // } else {
+    //   return false;
+    // }
   }
 
   const mappedSquares = squares.map((square) => {
